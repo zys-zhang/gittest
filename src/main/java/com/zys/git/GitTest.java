e0d56d2d7ee429d95e5018b6f2418dea824c744e
zys-zhang <2607575675@qq.com>
2022-05-05 16:37:19 +0800

    second commit

--- src/main/java/com/zys/git/GitTest.java
+++ src/main/java/com/zys/git/GitTest.java
@@ -7,5 +7,7 @@ package com.zys.git;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+
     }
 }
