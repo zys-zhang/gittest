0bdc36ece828e612a7f9063f2e839e95d4fd88e1
zys-zhang <2607575675@qq.com>
2022-05-05 16:35:03 +0800

    first commit

---
+++ src/main/java/com/zys/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zys.git;
+
+/**
+ * @author zys
+ * @date 2022/5/5- 16:30
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git!");
+    }
+}
