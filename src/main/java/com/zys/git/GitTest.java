8f96f16c2a3a6057f6f862d20e545d2999e42075
zys-zhang <2607575675@qq.com>
2022-05-07 16:23:44 +0800

    master commit2

--- src/main/java/com/zys/git/GitTest.java
+++ src/main/java/com/zys/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("master test!");
 
     }
 }
